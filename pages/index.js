e544c590176898ae07b82813bb36593529edb4d6
Siegfred Balona <siegblink@gmail.com>
2020-12-03 09:39:27 +0800

    Refactor 'index.js' file in 'pages' directory.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,19 @@
 import * as React from 'react';
 import Head from 'next/head';
 import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Fast Feedback</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feedback</h1>
-
-        <p className={styles.description}>
-          Current user:<code className={styles.code}>{auth?.user?.email}</code>
+      <main>
+        <h1>Fast Feedback</h1>
+        <p>
+          Current user:<code>{auth?.user?.email}</code>
         </p>
 
         {auth.user ? (
@@ -26,17 +22,6 @@ export default function Home() {
           <button onClick={(e) => auth.signinWithGitHub()}>Sign in</button>
         )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
