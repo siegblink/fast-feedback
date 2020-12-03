9182e0069855c2f33a7880554c0a5a427faa04da
Siegfred Balona <siegblink@gmail.com>
2020-12-03 23:54:15 +0800

    Update 'index.js' file within 'pages' directory.

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,39 @@
 import * as React from 'react';
 import Head from 'next/head';
-import { useAuth } from '../lib/auth';
+import { Button, Flex } from '@chakra-ui/react';
+import { FastFeedbackIcon } from 'public/icons';
+import { useAuth } from '@/lib/auth';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+      w="full"
+      minW="300px"
+    >
       <Head>
         <title>Fast Feedback</title>
       </Head>
-      <main>
-        <h1>Fast Feedback</h1>
-        <p>
-          Current user:<code>{auth?.user?.email}</code>
-        </p>
 
-        {auth.user ? (
-          <button onClick={(e) => auth.signout()}>Sign out</button>
-        ) : (
-          <button onClick={(e) => auth.signinWithGitHub()}>Sign in</button>
-        )}
-      </main>
-    </div>
+      <FastFeedbackIcon color="black.500" boxSize="64px" />
+      
+      {auth.user ? (
+        <Button onClick={(e) => auth.signout()}>Sign out</Button>
+      ) : (
+        <Button
+          mt={4}
+          size="sm"
+          onClick={(e) => auth.signinWithGitHub()}
+        >
+          Sign in
+        </Button>
+      )}
+    </Flex>
   );
 }
