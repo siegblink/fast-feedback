02fb0e1cf390498831104756e84a836cfd8216b4
Siegfred Balona <siegblink@gmail.com>
2020-12-03 23:56:25 +0800

    Setup chakra ui (#5)
    * Install 'chakra-ui' dependencies.
    
    * Setup the 'chakra-ui' theme.
    
    * Refactor 'index.js' file in 'pages' directory.
    
    * Use 'ChakraProvider' in '_app.js'.
    
    * Add '_document.js' in 'pages' directory.
    
    * Install '@chakra-ui/icons' package.
    
    * Setup 'compilerOptions' in 'jsconfig.json' file.
    
    * Add 'GlobalStyle' component in '_app.js'.
    
    * Add 'icons.js' file in 'public' directory.
    
    * Update 'index.js' file within 'pages' directory.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,42 @@
 import * as React from 'react';
-import { AuthProvider } from '../lib/auth';
-import '../styles/globals.css';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { AuthProvider } from '@/lib/auth';
+import { Global, css } from '@emotion/react';
+import customTheme from '@/styles/theme';
+import '@/styles/globals.css';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <React.Fragment>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      >
+        {children}
+      </Global>
+    </React.Fragment>
+  );
+};
 
 const App = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={customTheme}>
+      <GlobalStyle />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,39 @@
 import * as React from 'react';
 import Head from 'next/head';
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+import { Button, Flex } from '@chakra-ui/react';
+import { FastFeedbackIcon } from 'public/icons';
+import { useAuth } from '@/lib/auth';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+      w="full"
+      minW="300px"
+    >
       <Head>
         <title>Fast Feedback</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feedback</h1>
-
-        <p className={styles.description}>
-          Current user:<code className={styles.code}>{auth?.user?.email}</code>
-        </p>
-
-        {auth.user ? (
-          <button onClick={(e) => auth.signout()}>Sign out</button>
-        ) : (
-          <button onClick={(e) => auth.signinWithGitHub()}>Sign in</button>
-        )}
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <FastFeedbackIcon color="black.500" boxSize="64px" />
+      
+      {auth.user ? (
+        <Button onClick={(e) => auth.signout()}>Sign out</Button>
+      ) : (
+        <Button
+          mt={4}
+          size="sm"
+          onClick={(e) => auth.signinWithGitHub()}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+          Sign in
+        </Button>
+      )}
+    </Flex>
   );
 }
