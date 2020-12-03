7c722f9265f5a898978ee575ff3bf7af79deab73
Siegfred Balona <siegblink@gmail.com>
2020-12-03 09:40:03 +0800

    Use 'ChakraProvider' in '_app.js'.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { AuthProvider } from '../lib/auth';
+import theme from '../styles/theme';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 };
 
