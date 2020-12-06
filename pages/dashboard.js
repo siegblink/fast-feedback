07dead447ed215ee5eb604f884d21d5034cb5559
Siegfred Balona <siegblink@gmail.com>
2020-12-06 18:49:57 +0800

    Add 'Loading' component in 'dashboard.js' within 'pages' directory.

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,29 @@
 import * as React from 'react';
-import { useAuth } from '@/lib/auth';
+import { Flex, Heading } from '@chakra-ui/react';
+import { RepeatIcon } from '@chakra-ui/icons';
 import EmptyState from '@/components/EmptyState';
+import { useAuth } from '@/lib/auth';
+
+function Loading() {
+  return (
+    <Flex
+      height="100vh"
+      flexDirection="column"
+      justifyContent="flex-start"
+      alignItems="center"
+      backgroundColor="gray.100"
+    >
+      <RepeatIcon mt={100} boxSize={6} />
+      <Heading>Loading components</Heading>
+    </Flex>
+  );
+}
 
 export default function Dashboard() {
   const auth = useAuth();
 
   if (!auth.user) {
-    return 'Loading...';
+    return <Loading />;
   }
 
   return <EmptyState />;
